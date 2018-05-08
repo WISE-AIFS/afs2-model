6a9a7043c359368229cf15bf70dbbd8772a702c1
benchuang11046 <benchuang11046@gmail.com>
2018-05-09 00:19:50 +0800

    update test config handler test case

--- afs/tests/test_config_handler.py
+++ afs/tests/test_config_handler.py
@@ -6,9 +6,20 @@ flow_json_file='add_node.json'
 REQUEST="""
 {
 	"headers": {
-		"node_id": "ada49faf.e05cf",
-		"flow_id": "6805569a.b11218",
-		"host_url": ""
+		"Host": "140.92.24.91:9091",
+		"Connection": "keep-alive",
+		"Content-Length": "61",
+		"Origin": "chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop",
+		"Flow_id": "6805569a.b11218",
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36",
+		"Content-Type": "application/json",
+		"Cache-Control": "no-cache",
+		"Host_url": "123",
+		"Node_id": "ada49faf.e05cf",
+		"Postman-Token": "17190eb2-0bff-f66a-b146-1e96bc8ce97a",
+		"Accept": "*/*",
+		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7"
 	},
 	"body": {
 		"data": {
