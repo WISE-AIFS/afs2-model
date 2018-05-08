454619673a073e399ec97d26bb5c4daf349a8922
benchuang <benchuang@iii.org.tw>
2018-05-08 11:17:33 +0800

    update config handler

--- afs/config_handler.py
+++ afs/config_handler.py
@@ -11,28 +11,61 @@ class config_handler(object):
         self.param = []
         self.column = []
 
-    def set_flow(self, flow_json, node_info):
+    def set_flow(self, flow_json, request_body):
+        try:
+            request_body = json.loads(request_body)
+        except Exception as e:
+            raise AssertionError('Type error, request_body must be JSON')
+
         self.flow_obj = flow()
-        self.flow_obj.set_flow_config(node_info)
+        self.flow_obj.set_flow_config(request_body)
         self.flow_obj.get_flow_list_ab(flow_json)
         self.flow_obj.get_node_item(self.flow_obj.current_node_id)
 
     def get_param(self, key):
-        obj = self.flow_obj.current_node_obj[key]
-        if obj:
-            return obj
+        obj_value = self.flow_obj.current_node_obj[key]
+        obj_para = [x for x in self.param if x['name'] in key][0]
+        if obj_para['type'] in 'integer':
+            obj_value = int(obj_value)
+        elif obj_para['type'] in 'string':
+            obj_value = str(obj_value)
+        elif obj_para['type'] in 'float':
+            obj_value = float(obj_value)
+        else:
+            _logger.warning('Type has no specific type.')
+            obj_value = str(obj_value)
+
+        if obj_value:
+            return obj_value
         else:
             return None
 
+    def get_data(self, request_body):
+        try:
+            request_body = json.loads(request_body)
+            if request_body['data']:
+                return DataFrame.from_dict(request_body['data'])
+            else:
+                raise AssertionError('Data is not existed in request_body')
+        except Exception as e:
+            raise AssertionError('Type error, request_body must be JSON')
+
     def next_node(self,data):
-        self.flow_obj.exe_next_node(data)
+        if isinstance(data, DataFrame):
+            data = dict(data=data.to_dict())
+            self.flow_obj.exe_next_node(data)
+        else:
+            raise AssertionError('Type error, data must be Dataframe')
 
     def set_param(self, key, type='string', required=False ,default=None):
         param = {}
         param['name'] = key
-        param['tpye'] = type
+        param['type'] = type
         param['required'] = required
         param['default'] = default
+
+        # check if the name is the same, raise error
+
         self.param.append(param)
         pass
 
@@ -55,6 +88,7 @@ if __name__ == '__main__':
         flow_json = f.read()
     flow_json = json.loads(flow_json)
     req_body = {'data': {'value': {'0': 21}}, 'node_id': 'ada49faf.e05cf'}
+    req_body = json.dumps(req_body)
 
     cfg = config_handler()
     cfg.set_param('b', type='integer', required=True, default=10)
@@ -63,10 +97,9 @@ if __name__ == '__main__':
 
     cfg.set_flow(flow_json, req_body)
     b = cfg.get_param('b')
-    a = DataFrame.from_dict(req_body['data'])
-    result = a + int(b)
-    df_dict = dict(data=result.to_dict())
-    cfg.next_node(df_dict)
+    a = cfg.get_data(req_body)
+    result = a + b
+    cfg.next_node(result)
 
 
 
