ecd5c5ddb04124ab2ed8fe3625b0a0580e64ec4d
benchuang <benchuang@iii.org.tw>
2018-07-10 10:57:03 +0800

    version 1.2.8 testing

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'r
 install_requires = parse_requirements(requirements_path, session='hack')
 install_requires = [str(ir.req) for ir in install_requires]
 
-version_tag = subprocess.check_output(["git", "describe"]).strip().decode()
+# version_tag = subprocess.check_output(["git", "describe"]).strip().decode()
+version_tag = '1.2.8'
 
 setup(
     name='afs',
@@ -46,3 +47,8 @@ setup(
         ]
     },
 )
+
+# build wheel pacakge
+# command: python setup.py bdist_wheel
+# build tgz
+# command: python setup.py sdist
