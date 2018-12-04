f561c734e2747fd9b3517d66dc34736c67c06c8c
benchuang <benchuang11046@gmail.com>
2018-12-04 23:31:20 +0800

    add models v2 unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,4 @@
-import pytest
-import uuid
-import os
-from afs import models
-from afs import services
-from afs import config_handler
+import uuid, os, pytest
 from dotenv import load_dotenv
 
 env_path = os.path.join(os.path.dirname(__file__), ".env")
@@ -19,16 +14,19 @@ def data_dir():
 
 @pytest.fixture(scope='class')
 def models_resource():
+    from afs import models
     afs_models=models()
     yield afs_models
 
 @pytest.fixture(scope='class')
 def services_resource():
+    from afs import services
     afs_services=services()
     yield afs_services
 
 @pytest.fixture(scope='function')
 def config_handler_resource():
+    from afs import config_handler
     afs_config_handler = config_handler()
     yield afs_config_handler
 
