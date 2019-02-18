df5918b84cb77377f37c3a15f5e0d77db56b5c04
jacob <chenjr0719@gmail.com>
2019-02-18 14:16:24 +0800

    Implement refresh_sso_token

--- afs/clients/sso.py
+++ afs/clients/sso.py
@@ -1,5 +1,7 @@
 from urllib.parse import urljoin
 
+from jwt import decode as jwt_decode
+
 from .base import APISession
 from .exceptions import SSOException
 
@@ -25,5 +27,28 @@ class SSOClient:
 
         return token
 
+    def get_refresh_token(self, token):
+        decoded_token = jwt_decode(
+            token,
+            verify=False
+        )
+        refresh_token = decoded_token.get('refreshToken')
+        if not refresh_token:
+            raise SSOException('No refreshToken in response')
+        return refresh_token
+
     def refresh_sso_token(self, token):
-        pass
+        path = '{}/token'.format(self.api_version)
+        url = urljoin(self.api_endpoint, path)
+
+        refresh_token = self.get_refresh_token(token)
+        resp = self._session.post(
+            url,
+            json={'token': refresh_token}
+        )
+
+        token = resp.get('accessToken')
+        if not token:
+            raise SSOException('No accessToken in response')
+
+        return token
