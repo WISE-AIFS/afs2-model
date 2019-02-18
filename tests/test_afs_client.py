9ff8b33aa6f18d8438b12e83952fc06fe6261e9f
jacob <chenjr0719@gmail.com>
2019-02-18 13:30:35 +0800

    Add sso_token in afs_client test

--- tests/test_afs_client.py
+++ tests/test_afs_client.py
@@ -1,9 +1,25 @@
 import os
+from urllib.parse import urljoin
+
+import pytest
+import requests
 
 from afs import AFSClient
 from afs.clients.base import APISession
 from afs.clients.instances import InstancesClient
 from afs.clients.sso import SSOClient
+
+
+@pytest.fixture()
+def sso_token():
+    sso_endpoint = os.getenv('TEST_AFS_API_SERVER').replace('api-afs', 'portal-sso')
+    resp = requests.post(
+        urljoin(sso_endpoint, '/v2.0/auth/native'),
+        json={'username': os.getenv('TEST_USERNAME'), 'password': os.getenv('TEST_PASSWORD')}
+    )
+
+    token = resp.json()['accessToken']
+    yield token
 
 
 def test_afs_client():
@@ -34,12 +50,40 @@ def test_afs_client():
     assert isinstance(instances_client, InstancesClient)
 
 
+def test_afs_client_with_sso_token(sso_token):
+    afs_client = AFSClient(
+        api_endpoint=os.getenv('TEST_AFS_API_SERVER'),
+        token=sso_token
+    )
+
+    session = getattr(afs_client, 'session')
+    assert isinstance(session, APISession)
+
+    api_endpoint = getattr(afs_client, 'api_endpoint')
+    assert isinstance(api_endpoint, str)
+    assert api_endpoint == os.getenv('TEST_AFS_API_SERVER')
+
+    # Check SSO client
+    sso_client = getattr(afs_client, 'sso')
+    assert isinstance(sso_client, SSOClient)
+
+    # Check token and this token is set at session's headers
+    token = getattr(afs_client, 'token')
+    assert isinstance(token, str)
+    assert 'Bearer {}'.format(token) == session.headers.get('Authorization')
+
+    # Check instance client
+    instances_client = getattr(afs_client, 'instances')
+    assert isinstance(instances_client, InstancesClient)
+
+
 def test_get_api_info(afs_client):
     api_info = afs_client.get_api_info()
     assert isinstance(api_info, dict)
     assert 'API_version' in api_info
     assert api_info['API_version'] == 'v2'
     assert 'AFS_version' in api_info
+
 
 def test_get_api_version(afs_client):
     api_version = afs_client.get_api_version()
